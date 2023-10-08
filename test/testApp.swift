d327e1eb9d7fe6b5d7fd965bbea1de51be90dd46
Lujain <lujain.abh>
2023-10-08 10:10:47 +0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Lujain Abdullah Halabi Almeri on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
