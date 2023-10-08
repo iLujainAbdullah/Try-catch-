e4aa4a2fcf10a8921db024e0138fb88c9f319f88
Lujain <lujain.abh>
2023-10-08 11:57:11 +0300

    First commit

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -10,10 +10,49 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("c")
+                .resizable()
+                .frame(width: 500, height: 250)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.white, lineWidth: 6)).shadow(radius: 10)
+                .padding(5)
+            
+            
+            Text("Lujain Almeri").font(.largeTitle).fontWeight(.bold).foregroundColor(Color(hue: 0.381, saturation: 0.532, brightness: 0.454)).foregroundStyle(.brown).frame(maxWidth: .infinity, alignment: .center)
+        }
+        .padding()
+        
+        Text("I'm a fresh graduate Information Technology from KAU, specilized in Databases and Software Engineering. Currently, I'm a learner at Apple Developer Academy and I'm enjoying this journey.\nInterested in Business, design and marketing!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
+//
+//  ContentView.swift
+//  Riyadh
+//
+//  Created by Lujain Abdullah Halabi Almeri on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct test: View {
+    var body: some View {
+        VStack {
+            Image("c")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .cornerRadius(50)
+            
+                Text("Riyadh").font(.title).frame(maxWidth: .infinity, alignment: .leading)
+
+                                .frame(alignment: .top)
+
+                Text("Capital of Saudi Arabia")
+                    .foregroundStyle(.gray).frame(maxWidth: .infinity, alignment: .leading)
+            
         }
         .padding()
     }
